bcf69e0a951e304ecb1ba9baf2fff13c8cfdda6f
Jose D. Alvarez <jdalvarez23@gmail.com>
2018-09-27 22:28:09 -0500

    Update 09/27/2018 - updated if statement exercises

--- src/androiddeveloper/ifStatements.java
+++ src/androiddeveloper/ifStatements.java
@@ -10,11 +10,11 @@ package androiddeveloper;
  * @author jdalv
  */
 public class ifStatements {
-    
+
     // main method begins execution of Java application
     public static void main(String[] args) {
         int age = 35; // initialize and set integer value
-        
+
         if (age == 18) {
             System.out.println("User is 18");
         } else if (age < 18) {
@@ -22,18 +22,19 @@ public class ifStatements {
         } else {
             System.out.println("User is over 18");
         }
-        
+
         int[] array = {19, 28}; // initialize and set array values
-        
+
         // execute if second array integer is greater than the first array integer
         if (array[1] > array[0]) {
             System.out.println("The second integer (" + array[1] + ") is greater than the first integer (" + array[0] + ")");
-        // execute if second array integer is less than the first array integer
+            // execute if second array integer is less than the first array integer
         } else if (array[1] < array[0]) {
             System.out.println("The second integer (" + array[1] + ") is less than the first integer (" + array[0] + ")");
-        // execute if second array integer is equal to the first array integer
+            // execute if second array integer is equal to the first array integer
         } else {
             System.out.println("The integers are the same.");
-            
+
         }
+    }
 }
